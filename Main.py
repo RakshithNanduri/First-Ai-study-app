370c99531742ed1da9958cedcbf4da96895c80cc
rakshithgamer123456789-a11y <rakshithgamer123456789@gmail.com>
2026-07-04 18:34:36 +0530

    Delete Main.py

--- Main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="My Ai study App", page_icon="😊", layout="wide")
-col1, col2 = st.columns(2)
-with col1:
-    st.title("My Ai study App")
-    st.write("Welcome to the app!")
-with col2:
-    st.image('image.png', width=100, caption="My Ai study App", output_format="auto")
-    

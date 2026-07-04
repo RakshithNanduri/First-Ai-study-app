6393bd9bd59d0e68ee1d9a76146cf442f92d276e
Rakshith <Rakshithgamer123456789@email.com>
2026-07-04 09:56:09 +0530

    Testing

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import streamlit as st
-st.title("My Ai study App")
-question: str= st.text_input("Enter your question here")
-st.button("Submit")
-st.write(f"You asked: , {question}")
-
+col1, col2 = st.columns(2)
+with col1:
+    st.title("My Ai study App")
+    st.write("Welcome to the app!")
+with col2:
+    st.image("C:\\Users\\raksh\\OneDrive\\Desktop\\My first project\\image.png", width=100, caption="My Ai study App", output_format="auto")
+    

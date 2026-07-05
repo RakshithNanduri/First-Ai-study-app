331bd42956b575ffafa23351cf726defb6c9a0c0
Rakshith <Rakshithgamer123456789@email.com>
2026-07-05 10:14:56 +0530

    version 2 chat memory added

--- pages/1_Chat.py
+++ pages/1_Chat.py
@@ -1,19 +1,49 @@
 import streamlit as st
 import ollama as ol
-import pandas as pd
-prompt=''
 
-def ask_streamlit(prompt):
-    response=ol.chat(model=('qwen3:latest'), messages=[{'role': 'user', 'content': prompt}])
-    return response['message']['content']
 
-if 'messages' not in st.session_state:
-    st.chat_message('assistant').write('Hi! how can i assist you today??')
+def ask_ollama(messages):
+    response = ol.chat(model="qwen3:latest", messages=messages)
+    answer = response["message"]["content"]
+    return answer
 
-prompt = st.chat_input("Ask me anything", key="input")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Hey! How can I help you?"
+        }
+    ]
+
+
+for message in st.session_state.messages:
+    role = message["role"]
+    content = message["content"]
+
+    st.chat_message(role).write(content)
+
+
+prompt = st.chat_input("Ask me anything")
+
 
 if prompt:
+    user_message = {
+        "role": "user",
+        "content": prompt
+    }
+
+    st.session_state.messages.append(user_message)
+
     st.chat_message("user").write(prompt)
-    reply=ask_streamlit(prompt)
-    st.spinner("Wait for it...", show_time=True)
-    st.chat_message('assistant').write(reply)
+
+    reply = ask_ollama(st.session_state.messages)
+
+    assistant_message = {
+        "role": "assistant",
+        "content": reply
+    }
+
+    st.session_state.messages.append(assistant_message)
+
+    st.chat_message("assistant").write(reply)
